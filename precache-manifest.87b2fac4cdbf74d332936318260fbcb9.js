5676e7937ed13004b7b1b32256dd56db50d366ff
yamini465 <40388091+yamini465@users.noreply.github.com>
2020-07-10 17:47:22 +0530

    Updates

---
+++ precache-manifest.87b2fac4cdbf74d332936318260fbcb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd54bee0bf28d08f666143f19b58db03",
+    "url": "/Covid-19Tracker/index.html"
+  },
+  {
+    "revision": "4e11c21704ca0eb50d7a",
+    "url": "/Covid-19Tracker/static/css/main.ca5c6a46.chunk.css"
+  },
+  {
+    "revision": "bf95242d614d2187062d",
+    "url": "/Covid-19Tracker/static/js/2.c8f0af95.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19Tracker/static/js/2.c8f0af95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e11c21704ca0eb50d7a",
+    "url": "/Covid-19Tracker/static/js/main.0dd5937e.chunk.js"
+  },
+  {
+    "revision": "d55d597982e64a92a403",
+    "url": "/Covid-19Tracker/static/js/runtime-main.ea16c429.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19Tracker/static/media/image.d7265326.png"
+  }
+]);
